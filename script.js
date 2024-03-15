4f2cde536851821355603efe5d056dbd43c51540
musicextravaganzahsfc <163287710+musicextravaganzahsfc@users.noreply.github.com>
2024-03-15 21:58:28 +0000

    Aesthetics
    Tweak background, change descriptions, change title, changed header, improve parallax, improve responsiveness

--- script.js
+++ script.js
@@ -1,13 +1,6 @@
-var container = document.querySelector('.container');
-var contentContainer = document.getElementById('content_container');
+window.addEventListener("scroll", scrollBackground);
 
-container.addEventListener('scroll', function() {
-    // Adjust the scroll speed for the content container
-    var contentScrollSpeed = 0.5; // You can adjust this value as needed
-    
-    // Get the scroll position
-    var scrollPos = container.scrollTop;
-    
-    // Apply the scroll speed to the content container
-    contentContainer.style.transform = 'translateY(' + scrollPos * contentScrollSpeed + 'px)';
-});
+function scrollBackground() {
+  document.getElementById("starry_background").style.top =
+    (scrollY / 20) * -1 + `px`;
+}
